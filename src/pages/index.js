85909c4fe375c8bc3c9a558526d7ee541b371735
Waqas Mahmood <waqasnizamani3@gmail.com>
2020-11-10 21:01:48 +0500

    first committ

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,22 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (<div>
+    
+    <div>Hello world!</div>
+    <button className ="snipcart-add-item"
+        data-item-id="formal-shoe"
+        data-item-price="25"
+        data-item-description="This is best shoe"
+        data-item-url="/"
+        data-item-image="https://cdn.shopify.com/s/files/1/0104/5757/9583/products/OSCO-Men-Dress-Shoes-Men-Formal-Shoes-Leather-Luxury-Fashion-Wedding-Shoes-Men-Business-Casual-Oxford.jpg?v=1575045878"
+        data-item-name="Formal Shoe"
+        data-item-custom1-name="Size"
+        data-item-custom1-options="8|9[+5]|10[+10]"
+        data-item-custom2-name="Info"
+        data-item-custom2-type="checkbox"
+      >
+        Checkout
+      </button>
+    </div>)
 }
